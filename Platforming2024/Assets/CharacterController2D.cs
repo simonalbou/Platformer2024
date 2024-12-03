1eea6748c2064ecf076438b2f3e25a8c508f53eb
Simon Albou <albou.simon@gmail.com>
2024-12-03 15:36:33 +0100

    [Gameplay] Jump curve WIP

--- Platforming2024/Assets/CharacterController2D.cs
+++ Platforming2024/Assets/CharacterController2D.cs
@@ -5,9 +5,15 @@ public class CharacterController2D : MonoBehaviour
 {
     public float moveSpeed;
     public float gravity;
+    public AnimationCurve gravityMultiplierCurve;
 
     public Transform self;
     public CharacterRaycaster2D raycaster;
+
+    [System.NonSerialized] public bool isGrounded;
+    [System.NonSerialized] public bool isJumping;
+    [System.NonSerialized] public float jumpTimestamp;
+
 
     void Update()
     {
@@ -20,11 +26,43 @@ public class CharacterController2D : MonoBehaviour
 
         // check inputs
         movement.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        // check jump
+        if (Input.GetKeyDown(KeyCode.Z)) TryJump();        
+        float jumpMultiplier = 1;
+        if (isJumping)
+        {
+            // lire la courbe pour mettre à jour le jumpMultiplier
+            float timeSinceJumped = Time.time - jumpTimestamp;
+            jumpMultiplier = gravityMultiplierCurve.Evaluate(timeSinceJumped);
+
+            // si je découvre que je suis arrivé au bout de la courbe, on arrête le saut
+            float xMax = gravityMultiplierCurve.keys[gravityMultiplierCurve.keys.Length-1].time;
+            if (timeSinceJumped > xMax)
+            {
+                isJumping = false;
+            }
+        }
+
         // check gravity
-        movement.y = gravity * -1 * Time.deltaTime;
+        movement.y = gravity * jumpMultiplier * -1 * Time.deltaTime;
 
         // move accordingly
         Move(movement);
+    }
+
+    void TryJump()
+    {
+        // à moins d'un double-saut autorisé ou d'un coyote time actif, le saut doit échouer si on ne touche pas le sol
+        if (!isGrounded) return;
+
+        // le saut est autorisé : on initialise tout
+        isGrounded = false;
+        isJumping = true;
+        jumpTimestamp = Time.time;
+
+        // feedbacks divers ici : audio, VFX, etc etc
+        // et évidemment, l'animation de saut
     }
 
     void Move(Vector2 movement)
@@ -66,10 +104,16 @@ public class CharacterController2D : MonoBehaviour
         if (isThereCollision)
         {
             // unity event ou autre message visant à feedbacker la collision
+            if (movement < 0) isGrounded = true;
             return;
         }
         
-        // else :
+        // else : il n'y a pas collision
+
+        // j'ai réussi à bouger vers le bas : c'est que je tombe, donc que je ne touche pas (plus) le sol
+        if (movement < 0) isGrounded = false;
+        
+        // enfin, exécuter le mouvement
         self.Translate(Vector3.up * movement);
     }
 }
